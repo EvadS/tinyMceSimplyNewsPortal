4165b50ad444fec2a9eb87020cd4c44966e41a78
Evgeniy.Skiba <Evgeniy.Skiba@mobius-software.com>
2016-08-02 18:56:32 +0300

    keep working

---
+++ SimplyNewsPortal/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace SimplyNewsPortal.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SimplyNewsPortal.Models.BlogsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "SimplyNewsPortal.Models.BlogsContext";
+        }
+
+        protected override void Seed(SimplyNewsPortal.Models.BlogsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
