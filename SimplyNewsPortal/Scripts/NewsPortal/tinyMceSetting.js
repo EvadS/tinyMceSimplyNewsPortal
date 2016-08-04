a05caf39d3ad02731ebca0cc905a5f6da34db783
Evgeniy.Skiba <Evgeniy.Skiba@mobius-software.com>
2016-08-04 10:35:05 +0300

    first version

--- SimplyNewsPortal/Scripts/NewsPortal/tinyMceSetting.js
+++ SimplyNewsPortal/Scripts/NewsPortal/tinyMceSetting.js
@@ -1,14 +1,27 @@
 ﻿
 
-
     tinymce.init({
-    selector: "#Content",
-    theme: "modern",
-    menubar: false,
-    resize: false,
-    statusbar: false,
-    plugins: ["advlist autolink lists charmap preview hr anchor",
-        "pagebreak code nonbreaking table contextmenu directionality paste"],
-    toolbar1: "styleselect | bold italic underline | pagebreak code preview | undo redo",
-    toolbar2: "alignleft aligncenter alignright alignjustify | bullist numlist outdent indent"
-});
+        selector: "#Content",
+        width: '100%',
+        height: 600,
+        statusbar: false,
+        menubar: true,
+
+        setup: function (ed) {
+            ed.on('init', function () {
+                this.getDoc().body.style.fontSize = '14px';
+                this.getDoc().body.style.fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif';
+            });
+        },
+
+        paste_data_images: true,
+
+        plugins: [
+            "advlist autolink lists link base64_image charmap hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code",
+            "insertdatetime media nonbreaking save table contextmenu directionality",
+            "emoticons textcolor colorpicker textpattern "
+        ],
+        toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link base64_image media | forecolor backcolor"
+    });
+
